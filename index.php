416a979c4e2a70b1e1375a668df89af6e1a6030d
manik3115 <56823910+manik3115@users.noreply.github.com>
2019-10-22 16:41:04 +0800

    Update index.php
    added content

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<h1>Welcome</h1>
+<html>
+  <head>
+    <title>Test App</title>  
+  </head>
+  <body style="background:black">
+    <br/><br/>
+    <h1 align="center">Welcome </h1>
+  </body>
+</html>
